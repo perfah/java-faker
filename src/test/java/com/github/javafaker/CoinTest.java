65c69b691b0f4b35cef18f33ace6cd1303adbc15
Oliver Schwalbe Lehtihet <oliverlehtihet@gmail.com>
2020-03-02 23:47:22 +0100

    Better test to Coin to verify head or tails is actually returned

--- src/test/java/com/github/javafaker/CoinTest.java
+++ src/test/java/com/github/javafaker/CoinTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 
 import static com.github.javafaker.matchers.IsStringWithContents.isStringWithContents;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class CoinTest extends AbstractFakerTest {
 
     @Test
     public void testFlip() {
-        assertThat(faker.coin().flip(), isStringWithContents());
+        boolean valid;
+        String result;
+
+        for (int i = 0; i < 1000; i++) {
+            result = faker.coin().flip();
+            valid = result.equals("Heads") || result.equals("Tails");
+            assertTrue(valid);
+        }
+        
     }
 
 }
